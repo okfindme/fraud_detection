22c64ad15df6e25a04a83d7944dfe1417b461765
okfindme <adityajha2006714@gmail.com>
2026-05-07 03:36:49 +0530

    Initial commit

---
+++ app/db/session.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine("sqlite:///./dev.db")
+
+SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind = engine)
+
+# we dont call session here as session will be created even if we import it and cause the dataleaking...
